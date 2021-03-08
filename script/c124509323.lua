0945a976469d82ce79699139056fa37b966afcbe
StackSmashers169 <79965805+StackSmashers169@users.noreply.github.com>
2021-03-08 12:29:42 +1100

    Update c124509323.lua

--- script/c124509323.lua
+++ script/c124509323.lua
@@ -5,11 +5,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--token effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetOperation(s.ssuccess)
-	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -18,13 +13,10 @@ function s.initial_effect(c)
 	e2:SetCondition(s.spcon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
-	c:RegisterEffect(e2)
-end
-function s.ssuccess(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_RESET_TURN_SET+RESET_PHASE+PHASE_END,0,1)
+	c:RegisterEffect(e1)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_LINK
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_NORMAL) and IsLocation(LOCATION_GRAVE) and r==REASON_LINK
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
